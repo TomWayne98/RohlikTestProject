9795c4f6a2d242a0eabd3f3f6f0aacf38ba81b33
SmuggleBoi <pepa.hruska@gmail.com>
2020-10-29 22:01:22 +0100

    Initial files

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+
+plugins {
+    id(Plugins.androidApplication)
+    id(Plugins.kotlinAndroid)
+   // kotlin(Plugins.kapt)
+}
+
+android {
+    compileSdkVersion(Versions.targetSdk)
+
+    defaultConfig {
+        applicationId = ApplicationId.id
+        minSdkVersion(Versions.minSdk)
+        targetSdkVersion(Versions.targetSdk)
+        versionCode = Releases.versionCode
+        versionName = Releases.versionName
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    // without these lines the build would crash for older devices
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(Libraries.kotlinStd)
+    implementation(Libraries.kotlinKtx)
+    implementation(Libraries.appcompat)
+    implementation(Libraries.material)
+    implementation(Libraries.annotation)
+    implementation(Libraries.constraint)
+    implementation(Libraries.livedataKtx)
+    implementation(Libraries.viewModel)
+    implementation(Libraries.navigationFragment)
+    implementation(Libraries.navigationUi)
+    implementation(Libraries.koinViewModel)
+    implementation(Libraries.koinAndroid)
+}
---
+++ app/src/main/java/cz/tom/wayne/architecture/SingleLiveEvent.kt
@@ -0,0 +1,35 @@
+package cz.tom.wayne.architecture
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MediatorLiveData<T>() {
+
+    private val pending = AtomicBoolean()
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        pending.set(false) // prevent triggering at the start
+
+        super.observe(
+            owner, {
+                if (pending.compareAndSet(true, false)) {
+                    observer.onChanged(it)
+                }
+            }
+        )
+    }
+
+    @MainThread
+    override fun setValue(t: T?) {
+        pending.set(true)
+        super.setValue(t)
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
