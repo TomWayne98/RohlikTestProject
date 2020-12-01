7f10edd188e3d33f0351d125dc03518dc274a34e
SmuggleBoi <pepa.hruska@gmail.com>
2020-12-01 06:16:11 +0100

    Add debug comments

--- homescreen/src/main/java/cz/tom/wayne/homescreen/model/DogRepoImpl.kt
+++ homescreen/src/main/java/cz/tom/wayne/homescreen/model/DogRepoImpl.kt
@@ -7,21 +7,27 @@ import cz.tom.wayne.core.data.toDBEntity
 import cz.tom.wayne.core.repositories.DogRepo
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 
 class DogRepoImpl(private val dogApi: DogApi, private val dogDAO: DogDAO) : DogRepo {
 
     override suspend fun refreshRandomImage() {
         val result = dogApi.getDogImage().await()
+        Timber.d("Last dog image url downloaded")
         dogDAO.insertDogImage(result.toDBEntity())
     }
 
     override suspend fun getLastCachedDog() = flow<DogImageEntity> {
         dogDAO.getLastDog().collect {
+            Timber.d("Last dog collected: $it")
+
             // If there is no dog in DB we need to fetch one
             if (it.isNullOrEmpty()) {
                 refreshRandomImage()
+                Timber.d("Last dog is null or empty")
                 // emit(null)
             } else {
+                Timber.d("Last dog emitted")
                 emit(it.first())
             }
         }
