0a3ab786b0cfdb2db1a52f7848179fdb953f9248
SmuggleBoi <pepa.hruska@gmail.com>
2020-12-01 04:43:46 +0100

    Fix koin crash

--- app/src/main/java/cz/tom/wayne/App.kt
+++ app/src/main/java/cz/tom/wayne/App.kt
@@ -1,22 +1,25 @@
 package cz.tom.wayne
 
 import android.app.Application
+import cz.tom.wayne.di.dbModule
+import cz.tom.wayne.di.homeScreenModule
 import cz.tom.wayne.di.navigationModule
 import cz.tom.wayne.di.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(Level.NONE)
             androidContext(this@App)
             modules(
                 listOf(
-                    networkModule, navigationModule
+                    networkModule, navigationModule, dbModule, homeScreenModule
                 )
             )
         }
